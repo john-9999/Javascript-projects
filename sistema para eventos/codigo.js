02d59bcdbe3451365021c038302f4b4b3b6cd43a
John <93888821+john-9999@users.noreply.github.com>
2022-03-13 06:03:35 -0300

    Add files via upload

---
+++ sistema para eventos/codigo.js
@@ -0,0 +1,72 @@
+dinerojuan = prompt (" ¿Cuánto dinero tienes juan? ");
+dineroaxel = prompt (" ¿Cuánto dinero tienes axel? ");
+dineroAndrew = prompt (" ¿Cuánto dinero tienes Andrew? ");
+
+//juan//
+if (dinerojuan >=0.6 && dinerojuan <1 ){
+	alert ("juan, comprate un vaso de 500ml de Fernet Branca")
+}
+else if (dinerojuan >=1 && dinerojuan <1.6 ){
+	alert ("juan, comprate 1 copa de Gin Heredero")
+}
+else if (dinerojuan >= 1.6 && dinerojuan < 1.7 ){
+	alert ("juan, comprate 1 chandon")
+}
+else if (dinerojuan >=1.8 && dinerojuan < 2.9)
+{
+	alert("juan, comprate un red bull")
+}
+else if (dinerojuan >= 2.9)
+{
+	alert("juan, comprate quilmes bajo 0")
+}
+else{
+	alert("Lo siento juan, no hay productos para este monto")
+}
+
+//axel//
+
+if (dineroaxel >=0.6 && dineroaxel <1 ){
+	alert ("axel, comprate un vaso de 500ml de Fernet Branca")
+}
+else if (dineroaxel >=1 && dineroaxel <1.6 ){
+	alert ("axel, comprate 1 copa de Gin Heredero")
+}
+else if (dineroaxel >= 1.6 && dineroaxel < 1.7 ){
+	alert ("axel, comprate 1 chandon")
+}
+else if (dineroaxel >=1.8 && dineroaxel < 2.9)
+{
+	alert("axel, comprate un red bull")
+}
+else if (dineroaxel >= 2.9)
+{
+	alert("axel, comprate quilmes bajo 0")
+}
+else{
+	alert("Lo siento axel, no hay productos para este monto")
+}
+
+//Andrew//
+if (dineroAndrew >=0.6 && dineroAndrew <1 ){
+	alert ("Andrew, comprate un vaso de 500ml de Fernet Branca")
+}
+else if (dineroAndrew >=1 && dineroAndrew <1.6 ){
+	alert ("Andrew, comprate 1 copa de Gin Heredero")
+}
+else if (dineroAndrew >= 1.6 && dineroAndrew < 1.7 ){
+	alert ("juanAndrew, comprate 1 chandon")
+}
+else if (dineroAndrew >=1.8 && dineroAndrew < 2.9)
+{
+	alert("Andrew, comprate un red bull")
+}
+else if (dineroAndrew >= 2.9)
+{
+	alert("Andrew, comprate quilmes bajo 0")
+}
+else{
+	alert("Lo siento Andrew, no hay productos para este monto")
+}
+
+
