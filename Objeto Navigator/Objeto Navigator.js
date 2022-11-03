b7b4c2289d78061b26825f98fa8e404f50e98059
Juan <93888821+john-9999@users.noreply.github.com>
2022-11-03 19:18:44 -0300

    Add files via upload

---
+++ Objeto Navigator/Objeto Navigator.js
@@ -0,0 +1,27 @@
+console.log("AppCodeName: ",navigator.appCodeName);
+
+console.log("AppName: ",navigator.appName);
+
+console.log("AppVersion: ",navigator.appVersion);
+
+console.log("Connection",navigator.connection);
+
+console.log("Geolocation:",navigator.geolocation);
+
+console.log("HardwareConcurrency:",navigator.hardware);
+
+console.log("Languaje:",navigator.language);
+
+console.log("Languajes:",navigator.languages);
+
+console.log("MimeTypes:",navigator.mimeTypes);
+
+console.log("OnLine",navigator.onLine);
+
+console.log("UserAgemt:",navigator.userAgent);
+
+console.log("CookiesEnabled:",navigator.cookiesEnabled)
+
+console.log("Plataform:",navigator.plataform)
+
+
